0165276b3d46c03f9cc1fcc464b1a76f507b9ce8
Skazor <skage.kaupang@gmail.com>
2026-03-05 13:39:50 +0100

    La til hovedmeny med while-loop og switch i Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,36 @@
 ﻿using Kalkulator;
 
 Calculator calc = new Calculator();
+bool running = true;
 
-Console.WriteLine(calc.Add(3, 5));   
-Console.WriteLine(calc.Add(10, 2.5));
-Console.WriteLine(calc.Substract(10,3));
-Console.WriteLine(calc.Multiply(4, 5));
-Console.WriteLine(calc.Divide(10, 2));
+while (running)
+{
+    Console.WriteLine("\nVelg operasjon");
+    Console.WriteLine("+ Addisjon");
+    Console.WriteLine("- Substraksjon");
+    Console.WriteLine("* Multiplakasjon");
+    Console.WriteLine("/ Divisjon");
+    Console.WriteLine("Q Avslutt");
+    Console.WriteLine("\nDitt valg");
 
-List<double> tall = new List<double> {2, 3, 4};
-Console.WriteLine(calc.Add(tall));
-Console.WriteLine(calc.Substract(tall));
-Console.WriteLine(calc.Multiply(tall));
-Console.WriteLine(calc.Divide(tall));
+    string valg = Console.ReadLine()?.ToUpper() ?? "";
+
+    switch (valg)
+    {
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+            Console.WriteLine("Valgt: " + valg);
+            break;
+        case "Q":
+            Console.WriteLine("Avslutter...");
+            running = false;
+            break;
+        default:
+            Console.WriteLine("Ugyldig valg, prøv igjen.");
+            break;
+
+    }
+
+}

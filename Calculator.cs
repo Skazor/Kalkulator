7d66b91cec76671ab14553396ef4c98e5cd4184f
Skazor <skage.kaupang@gmail.com>
2026-03-05 13:09:23 +0100

    La til List-overloads for Add, Subtract, Multiply og Divide

--- Calculator.cs
+++ Calculator.cs
@@ -22,4 +22,40 @@ public class Calculator
         if (b == 0) throw new DivideByZeroException("Kan ikke dele på null!");
         return a / b;
     }
+
+    public double Add(List<double> tall)
+    {
+        double sum = 0;
+        foreach (double t in tall)
+            sum += t;
+        return sum;
+    }
+
+    public double Substract(List<double> tall)
+    {
+        double result = tall[0];
+        for (int i = 1; i < tall.Count; i++)
+            result -= tall[i];
+        return result;
+    }
+
+    public double Multiply(List<double> tall)
+    {
+        double result = tall[0];
+        for (int i = 1; i < tall.Count; i++)
+            result *= tall[i];
+        return result; 
+    }
+
+    public double Divide(List<double> tall)
+    {
+        double result = tall[0];
+        for (int i = 1; i < tall.Count; i++)
+        {
+            if (tall[i] == 0) throw new DivideByZeroException("Kan ikke dele på null");
+            result /= tall[i];
+        }
+        
+        return result;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -6,4 +6,10 @@ Console.WriteLine(calc.Add(3, 5));
 Console.WriteLine(calc.Add(10, 2.5));
 Console.WriteLine(calc.Substract(10,3));
 Console.WriteLine(calc.Multiply(4, 5));
-Console.WriteLine(calc.Divide(10, 2)); 
+Console.WriteLine(calc.Divide(10, 2));
+
+List<double> tall = new List<double> {2, 3, 4};
+Console.WriteLine(calc.Add(tall));
+Console.WriteLine(calc.Substract(tall));
+Console.WriteLine(calc.Multiply(tall));
+Console.WriteLine(calc.Divide(tall));

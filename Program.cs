b496223dbd9fc3377c966b61ff2222bf3b32faff
Skazor <skage.kaupang@gmail.com>
2026-03-05 12:20:18 +0100

    La til Subtract, Multiply og Divide metoder i Calculator

--- Program.cs
+++ Program.cs
@@ -3,4 +3,7 @@
 Calculator calc = new Calculator();
 
 Console.WriteLine(calc.Add(3, 5));   
-Console.WriteLine(calc.Add(10, 2.5)); 
+Console.WriteLine(calc.Add(10, 2.5));
+Console.WriteLine(calc.Substract(10,3));
+Console.WriteLine(calc.Multiply(4, 5));
+Console.WriteLine(calc.Divide(10, 2)); 
